3c8669e222a70d65a57b0becaab1bb06dcfaf8a6
Whygal <yanivgalili18@gmail.com>
2023-06-01 22:57:18 +0300

    some more

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { ContactUs } from './components/EmailForm/Emailjs';
 import Header from './components/EmailForm/Header/Header';
 import Footer from './components/Footer/Footer';
+import Main from './components/Main/Main';
 import "./style.css"
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Main/>
       <ContactUs/>
       <Footer/>
     </div>
---
+++ src/components/Main/Main.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+import "./style.css"
+const Main = () => {
+  return (
+    <div className='Main'>
+        <div className='texts'>
+      <Typography className='text'>
+Our company specializes in Fancy Color Diamond, Various kinds of colors, starting from the most common Yellow Diamonds to the rarest colors on earth such as Red, Orange, Pink, Blue, Green and Purple Diamonds.
+</Typography>
+<Typography className='text'>
+Our company has seniority in the color diamond industry for over 25 years, we’ve completed thousands of successful projects throughout the years, which earned us the reputation as the Professionals in the Fancy Color Diamonds and Fancy Color Diamond Jewelry industry.
+</Typography>
+<Typography className='text'>
+Tri Vivid Inc. is buying rough diamonds as well as polished diamonds and re-cutting them to upgrade the color for their maximum beauty, eventually diamonds are set in magnificent handmade jewelry, to complete its look and presentation.
+</Typography>
+<Typography className='text'>
+Tri Vivid Inc. is located in 580 5th Avenue in New York City
+</Typography>
+<Typography className='text'>
+Member of Diamond Dealers Clubs
+      </Typography>
+      </div>
+      <div className='logos'>
+      <img src='https://static.wixstatic.com/media/f35970_d384fc14590644d7a96cc432c5b595bf~mv2.png/v1/fill/w_289,h_110,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/f35970_d384fc14590644d7a96cc432c5b595bf~mv2.png' alt=''/>
+      <img src='https://static.wixstatic.com/media/f35970_a98e4cbe6aac4a4c91675417dd0190be~mv2.png/v1/fill/w_409,h_151,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/f35970_a98e4cbe6aac4a4c91675417dd0190be~mv2.png' alt=''/>
+      <img src='https://static.wixstatic.com/media/f35970_88a831db664544e9bc83e9a8dc98701f~mv2.png/v1/fill/w_375,h_143,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/f35970_88a831db664544e9bc83e9a8dc98701f~mv2.png' alt=''/>
+      </div>
+    </div>
+  )
+}
+
+export default Main
